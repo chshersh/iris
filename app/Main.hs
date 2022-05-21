27b91433b5e90abace4330c3f639357b38c5a305
Dmitrii Kovanikov <kovanikov@gmail.com>
2022-05-21 18:20:34 +0100

    [#6] Implement version parsers (#18)
    Resolves #6

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main (main) where
 import Control.Monad.IO.Class (MonadIO (..))
 
 import qualified Iris
+import qualified Paths_iris as Autogen
 
 
 newtype App a = App
@@ -11,8 +12,12 @@ newtype App a = App
 
 appSettings :: Iris.CliEnvSettings () ()
 appSettings = Iris.defaultCliEnvSettings
-    { Iris.cliEnvSettingsHeaderDesc = "Iris usage example"
-    , Iris.cliEnvSettingsProgDesc   = "A simple grep utility"
+    { Iris.cliEnvSettingsHeaderDesc      = "Iris usage example"
+    , Iris.cliEnvSettingsProgDesc        = "A simple grep utility"
+    , Iris.cliEnvSettingsVersionSettings =
+        Just (Iris.defaultVersionSettings Autogen.version)
+            { Iris.versionSettingsMkDesc = \v -> "Iris Example v" <> v
+            }
     }
 
 app :: App ()
--- src/Iris/Env.hs
+++ src/Iris/Env.hs
@@ -13,19 +13,81 @@ Environment of a CLI app.
 
 
 module Iris.Env
-    ( -- * CLI application environment
-      CliEnv (..)
-
-      -- * Settings for the CLI app
-    , CliEnvSettings (..)
+    ( -- * Settings for the CLI app
+      -- ** Global CLI settings
+      CliEnvSettings (..)
     , defaultCliEnvSettings
+      -- ** Application version settings
+    , VersionSettings (..)
+    , defaultVersionSettings
+
+      -- * CLI application environment
+    , CliEnv (..)
     , mkCliEnv
     ) where
 
 import Data.Kind (Type)
+import Data.Version (Version, showVersion)
 
 import qualified Options.Applicative as Opt
 
+
+{- |
+
+@since 0.0.0.0
+-}
+data CliEnvSettings (cmd :: Type) (appEnv :: Type) = CliEnvSettings
+    {  -- | @since 0.0.0.0
+      cliEnvSettingsCmdParser       :: Opt.Parser cmd
+
+      -- | @since 0.0.0.0
+    , cliEnvSettingsAppEnv          :: appEnv
+
+      -- | @since 0.0.0.0
+    , cliEnvSettingsHeaderDesc      :: String
+
+      -- | @since 0.0.0.0
+    , cliEnvSettingsProgDesc        :: String
+
+      -- | @since 0.0.0.0
+    , cliEnvSettingsVersionSettings :: Maybe VersionSettings
+    }
+
+
+{- |
+
+@since 0.0.0.0
+-}
+defaultCliEnvSettings :: CliEnvSettings () ()
+defaultCliEnvSettings = CliEnvSettings
+    { cliEnvSettingsCmdParser       = pure ()
+    , cliEnvSettingsAppEnv          = ()
+    , cliEnvSettingsHeaderDesc      = "Simple CLI program"
+    , cliEnvSettingsProgDesc        = "CLI tool build with iris - a Haskell CLI framework"
+    , cliEnvSettingsVersionSettings = Nothing
+    }
+
+{- |
+
+@since 0.0.0.0
+-}
+data VersionSettings = VersionSettings
+    { -- | @since 0.0.0.0
+      versionSettingsVersion :: Version
+
+      -- | @since 0.0.0.0
+    , versionSettingsMkDesc  :: String -> String
+    }
+
+{- |
+
+@since 0.0.0.0
+-}
+defaultVersionSettings :: Version -> VersionSettings
+defaultVersionSettings version = VersionSettings
+    { versionSettingsVersion = version
+    , versionSettingsMkDesc  = id
+    }
 
 {- | CLI application environment. It contains default settings for
 every CLI app and parameter
@@ -50,36 +112,6 @@ data CliEnv (cmd :: Type) (appEnv :: Type) = CliEnv
 
 @since 0.0.0.0
 -}
-data CliEnvSettings (cmd :: Type) (appEnv :: Type) = CliEnvSettings
-    {  -- | @since 0.0.0.0
-      cliEnvSettingsCmdParser  :: Opt.Parser cmd
-
-      -- | @since 0.0.0.0
-    , cliEnvSettingsAppEnv     :: appEnv
-
-      -- | @since 0.0.0.0
-    , cliEnvSettingsHeaderDesc :: String
-
-      -- | @since 0.0.0.0
-    , cliEnvSettingsProgDesc   :: String
-    }
-
-{- |
-
-@since 0.0.0.0
--}
-defaultCliEnvSettings :: CliEnvSettings () ()
-defaultCliEnvSettings = CliEnvSettings
-    { cliEnvSettingsCmdParser   = pure ()
-    , cliEnvSettingsAppEnv      = ()
-    , cliEnvSettingsHeaderDesc  = "Simple CLI program"
-    , cliEnvSettingsProgDesc    = "CLI tool build with iris - a Haskell CLI framework"
-    }
-
-{- |
-
-@since 0.0.0.0
--}
 mkCliEnv
     :: forall cmd appEnv
     .  CliEnvSettings cmd appEnv
@@ -92,8 +124,34 @@ mkCliEnv CliEnvSettings{..} = do
         }
   where
     cmdParserInfo :: Opt.ParserInfo cmd
-    cmdParserInfo = Opt.info (Opt.helper <*> cliEnvSettingsCmdParser) $ mconcat
-        [ Opt.fullDesc
-        , Opt.header cliEnvSettingsHeaderDesc
-        , Opt.progDesc cliEnvSettingsProgDesc
-        ]
+    cmdParserInfo = Opt.info
+        ( Opt.helper
+        <*> mkVersionParser cliEnvSettingsVersionSettings
+        <*> cliEnvSettingsCmdParser
+        )
+        $ mconcat
+            [ Opt.fullDesc
+            , Opt.header cliEnvSettingsHeaderDesc
+            , Opt.progDesc cliEnvSettingsProgDesc
+            ]
+
+mkVersionParser :: Maybe VersionSettings -> Opt.Parser (a -> a)
+mkVersionParser = maybe (pure id) fullVersionP
+
+fullVersionP :: VersionSettings -> Opt.Parser (a -> a)
+fullVersionP VersionSettings{..} = versionP <*> numericVersionP
+  where
+    versionStr :: String
+    versionStr = showVersion versionSettingsVersion
+
+    versionP :: Opt.Parser (a -> a)
+    versionP = Opt.infoOption (versionSettingsMkDesc versionStr) $ mconcat
+       [ Opt.long "version"
+       , Opt.help "Show application version"
+       ]
+
+    numericVersionP :: Opt.Parser (a -> a)
+    numericVersionP = Opt.infoOption versionStr $ mconcat
+       [ Opt.long "numeric-version"
+       , Opt.help "Show only numeric application version"
+       ]
